0e351184776677e932b848463e5017c01ea1c3c2
mrvnpas <153577829+mrvnpas@users.noreply.github.com>
2024-01-22 14:47:20 +0900

    Add files via upload

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,12 +1,28 @@
 package com.example.demo;
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class TestController {
+	public final TestRepository testRepository;
 	@GetMapping("/api/v1/customers")
 	public String findAllCustomers(){
-		return "a";
+		List<TestEntity> testList = testRepository.findAll();
+		if(testList.isEmpty()) {
+			TestEntity testEntity = new TestEntity();
+			testEntity.setName("aaaa");
+			testRepository.saveAndFlush(testEntity);
+		}
+		testList = testRepository.findAll();
+		String result = testList.stream().map(TestEntity::toString).collect(Collectors.joining(","));
+		return result;
 	}
 }
